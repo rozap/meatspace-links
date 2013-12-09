91220398bc733d12811f93188fcb8b1308d63c0e
rozap <chrisd1891@gmail.com>
2013-12-08 23:22:35 -0800

    this is taking too long and i am impatient

--- find_musics.py
+++ find_musics.py
@@ -15,7 +15,11 @@ def main():
 
 	for l in links:
 		print l
-		g.do_the_thing(l['key'], l['message'])
+		if 'youtube' in l['message'] or 'soundcloud' in l['message']:
+			g.do_the_thing(l['key'], l['message'])
+		else:
+            d.insert_link(l['key'], l['message'], l['link'], False)
+
 
 if __name__ == '__main__':
 	main()
