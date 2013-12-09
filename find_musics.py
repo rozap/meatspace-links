a9d44b6202ad2fc7321b52bce5fa0170360bb4ba
rozap <chrisd1891@gmail.com>
2013-12-08 23:23:57 -0800

    i am not sophistacted

--- find_musics.py
+++ find_musics.py
@@ -4,22 +4,22 @@ from listener import LinkGetter
 
 
 def main():
-	d = Database()
-	g = LinkGetter()
+    d = Database()
+    g = LinkGetter()
 
-	links = d.get_links(limit = 100000)
+    links = d.get_links(limit = 100000)
 
-	cur = d._db.cursor()
-	cur.execute("DELETE FROM links")
-	d._db.commit()
+    cur = d._db.cursor()
+    cur.execute("DELETE FROM links")
+    d._db.commit()
 
-	for l in links:
-		print l
-		if 'youtube' in l['message'] or 'soundcloud' in l['message']:
-			g.do_the_thing(l['key'], l['message'])
-		else:
+    for l in links:
+        print l
+        if 'youtube' in l['message'] or 'soundcloud' in l['message']:
+            g.do_the_thing(l['key'], l['message'])
+        else:
             d.insert_link(l['key'], l['message'], l['link'], False)
 
 
 if __name__ == '__main__':
-	main()
+    main()
